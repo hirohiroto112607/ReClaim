7c8ababa2e8fc542098b6c6e9556e2153471850d
hirohiroto112607 <113352696+hirohiroto112607@users.noreply.github.com>
2024-12-19 16:07:04 +0900

    ログアウト機能を更新し、ログアウト用のビューを追加。ログアウトページのフォームを修正し、ユーザー認証に基づくナビゲーションリンクを更新。

--- reclaim/accounts/urls.py
+++ reclaim/accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path('signup/', views.SignupView.as_view(), name="signup"),
     path('login/', views.LoginView.as_view(), name="login"),
-    path('logout/', views.LogoutView.as_view(), name="logout"),
+    path('logout/',views.logout_get, name="logout"),
     path('profile/', views.profile, name="profile"),
 ]
--- reclaim/accounts/views.py
+++ reclaim/accounts/views.py
@@ -47,3 +47,6 @@ class LogoutView(BaseLogoutView):
     
 def profile(request):
     return render(request, "accounts/profile.html")
+
+def logout_get(request):
+    return render(request, "accounts/logout.html")
