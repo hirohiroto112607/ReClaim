2a27704fa47f3d8f02a3a8cd635dbff05153c32a
hirohiroto112607 <113352696+hirohiroto112607@users.noreply.github.com>
2024-12-19 12:45:31 +0900

    ログイン機能を追加し、サインアップページを更新

--- reclaim/accounts/views.py
+++ reclaim/accounts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib.auth import login, authenticate
-from django.views.generic.edit import CreateView 
+from django.views.generic.edit import CreateView
 from .forms import SignUpForm
 from django.views.generic import TemplateView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
+
 
 # Create your views here.
 
@@ -14,9 +20,9 @@ class IndexView(TemplateView):
 
 class SignupView(CreateView):
     """ ユーザー登録用ビュー """
-    form_class = SignUpForm # 作成した登録用フォームを設定
-    template_name = "accounts/signup.html" 
-    success_url = reverse_lazy("accounts:index") # ユーザー作成後のリダイレクト先ページ
+    form_class = SignUpForm  # 作成した登録用フォームを設定
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("accounts:index")  # ユーザー作成後のリダイレクト先ページ
 
     def form_valid(self, form):
         # ユーザー作成後にそのままログイン状態にする設定
@@ -26,3 +32,16 @@ class SignupView(CreateView):
         user = authenticate(account_id=account_id, password=password)
         login(self.request, user)
         return response
+
+
+class LoginFrom(AuthenticationForm):
+    class Meta:
+        model = User
+
+
+class LoginView(BaseLoginView):
+    form_class = LoginFrom
+    template_name = "accounts/login.html"
+
+class LogoutView(BaseLogoutView):
+    success_url = reverse_lazy("accounts:index")
