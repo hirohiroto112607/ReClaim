a09e1a093c726f40c552896e15a1013085f0f2d1
hirohiroto112607 <113352696+hirohiroto112607@users.noreply.github.com>
2025-01-28 14:53:12 +0900

    機能改善: itemモデルにitem_keywordフィールドを追加し、不要なitem_keywordモデルを削除しました。また、contactビューのフォーム処理を整理しました。

--- reclaim/items/models.py
+++ reclaim/items/models.py
@@ -13,6 +13,7 @@ class item(models.Model):
     item_date = models.DateTimeField()
     item_lost_location = models.CharField(max_length=100)
     item_description = models.TextField()
+    item_keyword = models.CharField(max_length=200)
     item_image = models.ImageField(upload_to='images/', blank=True, null=True)
     item_status = models.BooleanField(default=False)
     item_founder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
@@ -49,9 +50,9 @@ class item_message(models.Model):
     def __str__(self):
         return str(self.message)
     
-class item_keyword(models.Model):
-    keyword_id = models.AutoField(primary_key=True)
-    item_id = models.ForeignKey('item', on_delete=models.CASCADE)
-    keyword = models.CharField(max_length=200)
-    def __str__(self):
-        return str(self.keyword)
+# class item_keyword(models.Model):
+#     keyword_id = models.AutoField(primary_key=True)
+#     item_id = models.ForeignKey('item', on_delete=models.CASCADE)
+#     keyword = models.CharField(max_length=200)
+#     def __str__(self):
+#         return str(self.keyword)
