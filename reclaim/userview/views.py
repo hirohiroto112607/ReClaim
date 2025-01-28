c5924746dcf99d689f3feab688832a9da95501f4
hirohiroto112607 <113352696+hirohiroto112607@users.noreply.github.com>
2025-01-28 14:53:12 +0900

    機能改善: スクロールスタイルを追加し、overviewテンプレートから不要なスタイルを削除しました。また、jQueryのスクリプトを整理しました。

--- reclaim/userview/views.py
+++ reclaim/userview/views.py
@@ -32,33 +32,26 @@ def detail_item_view(request, pk):
     return render(request, 'userview/item_detail.html', {'item': item_instance})
 
 
-
 def contact(request, pk):
-    # if this is a POST request we need to process the form data
     if request.method == "POST":
-        # create a form instance and populate it with data from the request:
         form = ItemContactForm(request.POST)
-        # check whether it's valid:
         if form.is_valid():
-            query_string = request.body.decode()
-            parsed_query = parse_qs(query_string)
-            print(parsed_query)
-            # process the data in form.cleaned_data as required
-            email = parsed_query['email'][0]
-            item_id = parsed_query['item_id'][0]
-            message = parsed_query['message'][0]
-            item_instance = item.objects.get(pk=item_id)  # ここでitemインスタンスを取得
+            email = request.POST.get('email')
+            item_id = request.POST.get('item_id')
+            message = request.POST.get('message')
+            item_instance = item.objects.get(pk=item_id)
             item_message.objects.create(
-                item_id=item_instance,  # itemインスタンスを割り当てる
+                item_id=item_instance,
                 message=message,
                 email=email
             )
-        # ...
-        # redirect to a new URL:
-        return HttpResponseRedirect("/userview/")
+            return redirect('userview:detail', pk=item_id)
+        else:
+            # フォームが無効な場合、エラーメッセージを表示
+            item_instance = get_object_or_404(item, pk=pk)
+            return render(request, 'userview/contact.html', {'form': form, 'item_instance': item_instance})
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = ItemContactForm()
         item_instance = get_object_or_404(item, pk=pk)
-    return render(request, 'userview/contact.html', {'item_instance': item_instance})
+    return render(request, 'userview/contact.html', {'form': form, 'item_instance': item_instance})
