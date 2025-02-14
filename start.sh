6ab5c56c4142766a84a1b5b55c94b438bbce340e
hirohiroto112607 <hirohiroto112607@f5.si>
2025-02-14 04:41:13 +0000

    不要なファイルを削除

--- start.sh
+++ start.sh
@@ -15,16 +15,18 @@ cleanup() {
 # SIGINTとSIGTERMのトラップを設定
 trap cleanup SIGINT SIGTERM
 
+conda activate reclaim
+
 # cloudflaredをバックグラウンドで実行
-nohup cloudflared tunnel run reclaim &
+cloudflared tunnel run reclaim &
 pids+=($!)
 
 # Django開発サーバーをバックグラウンドで実行
-nohup python manage.py runserver &
+python manage.py runserver &
 pids+=($!)
 
 # Django background tasksをバックグラウンドで実行
-nohup python manage.py process_tasks &
+python manage.py process_tasks &
 pids+=($!)
 
 echo "全てのサービスを開始しました"
